3bb06d0eaf10d2f2d0c08fb50a9d03b948aedb49
AnalGonzalez <mirkitopokemon@gmail.com>
2017-06-21 15:19:01 +0200

    Se crea este nuevo Form para reemplazar el anterior y obsoleto.
    Form2.cs
    
    con nuevo contenido y aplicaciones, dispuesto.

---
+++ Form2.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Git_Ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+			// Modificación desde otro repositorio y otro usuario
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saludos desde ¡Hola Huelva!");
+			
+			// Modificación en 2015
+			
+			// Modificación en 2016
+			
+			// Segunda modificación 2016 en clase
+      
+      //Primera Modificacion en 2017
+        }
+    }
+}
